b934eb18ef7943479a2a68ed99b38218d3171b65
naimatofficial <naimat.official.10@gmail.com>
2025-09-15 11:19:01 +0500

    setup initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log('Hello DevOps');
+
+console.log("I'm learning devops pipelines");
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log('Start test');
+setTimeout(() => console.log('3 seconds timer'), 3000);
+console.log('End test');
